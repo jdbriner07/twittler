1d1436ecc11cddf81b592be79e0ac0205b7b2944
Jeffrey Briner <jdbriner07@gmail.com>
2017-01-29 15:50:41 -0800

    got time to display in a format I wanted

--- JeffTwittler.js
+++ JeffTwittler.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
     while(index >= 0){
         var tweet = streams.home[index];
         var $tweet = $('<div></div>');
-        console.log(JSON.stringify(tweet.created_at));
-        $tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + tweet.created_at);
+        var time = timeStampRead(tweet.created_at);
+        $tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + time);
         $tweet.appendTo($body);
         index -= 1;
     }
@@ -22,7 +22,8 @@ $('.recent').on('click', function(){
     	while(maxTwittles >= 0){
         	var tweet = streams.home[index];
         	var $tweet = $('<div></div>');
-        	$tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + tweet.created_at);
+            var time = timeStampRead(tweet.created_at);
+        	$tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + time);
         	$tweet.appendTo($body);
         	index -= 1;
             maxTwittles -= 1;
@@ -37,12 +38,21 @@ $('.all').on('click', function(){
         while(index >= 0){
             var tweet = streams.home[index];
             var $tweet = $('<div></div>');
-            $tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + tweet.created_at);
+            var time = timeStampRead(tweet.created_at);
+            $tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + time);
             $tweet.appendTo($body);
             index -= 1;
         }
 });
 
 var timeStampRead = function(timeStamp) {
-    return timeStamp.substring(11,15) + "    " + timeStamp.substring(5,7) + "/" + timeStamp.substring(8,10) + "/" + timeStamp.substring(0,4);
+    var string = JSON.stringify(timeStamp);
+    var hours = parseFloat(string.substring(12,14));
+    if (hours >= 8) {
+        hours -=8;
+    }
+    else {
+        hours += 16;
+    }
+    return hours + string.substring(14,17) + "    " + string.substring(6,8) + "/" + string.substring(9,11) + "/" + string.substring(1,5);
 }
