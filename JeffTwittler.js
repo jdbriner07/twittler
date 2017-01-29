04643110245f388014155f0b1f505e7df2753aca
Jeffrey Briner <jdbriner07@gmail.com>
2017-01-29 15:05:31 -0800

    added a few features improved a bit

--- JeffTwittler.js
+++ JeffTwittler.js
@@ -16,18 +16,34 @@ $(document).ready(function(){
 
 
 
-$('button').on('click', function(){
+$('.recent').on('click', function(){
 		var $body = $('.twittlers');
     	$body.html('');
+        var maxTwittles = 10;
 
     	var index = streams.home.length - 1;
-    	while(index >= 0){
+    	while(maxTwittles >= 0){
         	var tweet = streams.home[index];
         	var $tweet = $('<div></div>');
         	$tweet.text('@' + tweet.user + ': ' + tweet.message);
         	$tweet.appendTo($body);
         	index -= 1;
+            maxTwittles -= 1;
     	}
+});
+
+$('.all').on('click', function(){
+        var $body = $('.twittlers');
+        $body.html('');
+
+        var index = streams.home.length - 1;
+        while(index >= 0){
+            var tweet = streams.home[index];
+            var $tweet = $('<div></div>');
+            $tweet.text('@' + tweet.user + ': ' + tweet.message);
+            $tweet.appendTo($body);
+            index -= 1;
+        }
 });
 
 
