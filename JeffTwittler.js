784ef1b8c1589ae24a4034fadb8a86a2b63d35ad
Jeffrey Briner <jdbriner07@gmail.com>
2017-01-29 18:49:14 -0800

    got the function to look at a person's twittlers working

--- JeffTwittler.js
+++ JeffTwittler.js
@@ -6,42 +6,60 @@ $(document).ready(function(){
     while(index >= 0){
         var tweet = streams.home[index];
         var $tweet = $('<div></div>');
+        var $user = $('<button type="button"></button>');
+        $user.addClass(tweet.user);
+        $user.attr("ID","awesome");
+        $user.text('@' + tweet.user);
         var time = timeStampRead(tweet.created_at);
-        $tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + time);
+        $tweet.text(': ' + tweet.message + "; " + time);
+        //$tweet.addClass(tweet.user);
+        $user.appendTo($body);
         $tweet.appendTo($body);
         index -= 1;
     }
 });
 
-$('.recent').on('click', function(){
+$('.recent').click(function(){
 		var $body = $('.twittlers');
     	$body.html('');
         var maxTwittles = 10;
-
     	var index = streams.home.length - 1;
     	while(maxTwittles >= 0){
         	var tweet = streams.home[index];
         	var $tweet = $('<div></div>');
+            var $user = $('<button type="button"></button>');
+            $user.addClass(tweet.user);
+            $user.attr("ID","awesome");
+            $user.text('@' + tweet.user);
             var time = timeStampRead(tweet.created_at);
-        	$tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + time);
+        	$tweet.text(': ' + tweet.message + "; " + time);
+            $tweet.addClass(tweet.user);
+            $user.appendTo($body);
         	$tweet.appendTo($body);
         	index -= 1;
             maxTwittles -= 1;
     	}
 });
 
-$('.all').on('click', function(){
+$('.all').click(function(){
         var $body = $('.twittlers');
         $body.html('');
-
+        var maxTwittles = 10;
         var index = streams.home.length - 1;
         while(index >= 0){
             var tweet = streams.home[index];
             var $tweet = $('<div></div>');
+            var $user = $('<button type="button"></button>');
+            $user.addClass(tweet.user);
+            $user.attr("ID","awesome");
+            $user.text('@' + tweet.user);
             var time = timeStampRead(tweet.created_at);
-            $tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + time);
+            $tweet.text(': ' + tweet.message + "; " + time);
+            $tweet.addClass(tweet.user);
+            $user.appendTo($body);
             $tweet.appendTo($body);
             index -= 1;
+            maxTwittles -= 1;
         }
 });
 
@@ -56,3 +74,20 @@ var timeStampRead = function(timeStamp) {
     }
     return hours + string.substring(14,17) + "    " + string.substring(6,8) + "/" + string.substring(9,11) + "/" + string.substring(1,5);
 }
+
+$(document).on("click", "#awesome", function(){
+            var $body = $('.twittlers');
+            $('div').text('');
+            console.log("button pressed")
+            var index = streams.home.length - 1;
+            while(index >= 0){
+                var tweet = streams.home[index];
+                var $tweet = $('<div></div>');
+                var time = timeStampRead(tweet.created_at);
+                $tweet.text('@' + tweet.user + ': ' + tweet.message + "; " + time);
+                if (tweet.user === $(this).attr("class")) {
+                    $tweet.appendTo($body);
+        }
+        index -= 1;
+    }
+});
